f56c0b66a5f96a4bb0011263e11595d78c975cff
Antoni Kowalewski <kowalewski.antek@gmail.com>
2021-08-31 00:49:04 +0200

    Add OrderForm and OrderSummary final version

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 
-const OrderForm = () => (
+const OrderForm = (props) => (
 
   <Row>
     <Col xs={12}>
-      <OrderSummary/>
+      <OrderSummary
+        options = {props.options}
+        tripCost = {props.tripCost}
+      />
     </Col>
   </Row>
 
 );
+
+OrderForm.propTypes = {
+  options: PropTypes.object,
+  tripCost: PropTypes.string,
+};
 
 export default OrderForm;
--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './OrderSummary.scss';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
 
-const OrderSummary = () => (
+const OrderSummary = (props) => (
 
   <h2 className={styles.component}>
-    Total: <strong>$12,345</strong>
+    Total: <strong>{formatPrice(calculateTotal(props.tripCost, props.options))}</strong>
   </h2>
 
 );
+
+OrderSummary.propTypes = {
+  options: PropTypes.object,
+  tripCost: PropTypes.string,
+};
 
 export default OrderSummary;
